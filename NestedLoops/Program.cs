6c23c7539917eeef7a169d92b907863682e7e567
jakobEk <ek.jacke@gmail.com>
2016-10-14 12:15:12 +0200

    Övn1-2

--- NestedLoops/Program.cs
+++ NestedLoops/Program.cs
@@ -10,6 +10,55 @@ namespace NestedLoops
     {
         static void Main(string[] args)
         {
+            /* Övning 1  
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    Console.Write("o#");
+                }
+                Console.WriteLine();
+            }
+            */
+            /* Övning 2
+            for (int i = 0; i < 11; i++)
+            {
+                int modulo = i % 2;
+                if (modulo == 0) {
+                    for (int x = 0; x < 11; x++)
+                    {
+                        int modulo2 = x % 2;
+                        if (modulo2 == 0)
+                        {
+                            Console.Write("o");
+                        }
+                        else
+                        {
+                            Console.Write("#");
+                        }
+                    }
+                }
+                else
+                {
+                    for (int y = 0; y < 11; y++)
+                    {
+                        int modulo3 = y % 2;
+                        if (modulo3 ==0)
+                        {
+                            Console.Write("#");
+                        }
+                        else
+                        {
+                            Console.Write("o");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            */
+
+
+            Console.ReadKey();
         }
     }
 }
